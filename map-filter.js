86affc8bdc75fc7a4b7a168c30e69e9f7d6a604b
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-02-03 00:32:52 +0600

    node

---
+++ map-filter.js
@@ -0,0 +1,9 @@
+const numbers = [3,5,6,2,7]
+const output =[]
+for (i=0;i<numbers.length;i++){
+          const element = numbers[i];
+          const  result = element*element;
+          output.push(result);
+          
+}
+console.log(output);
---
+++ null-undefiend.js
@@ -0,0 +1,6 @@
+function add (num1,num2){
+          return (num1+num2);
+          
+}
+var result  =  add(20,25);
+console.log(result);
