a7f187fcfca49fcb417fc292c23c97e924fffe1d
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-02-03 00:43:17 +0600

    hooodie

--- map-filter.js
+++ map-filter.js
@@ -1,9 +1,14 @@
-const numbers = [3,5,6,2,7]
-const output =[]
-for (i=0;i<numbers.length;i++){
-          const element = numbers[i];
-          const  result = element*element;
-          output.push(result);
-          
-}
-console.log(output);
+// const numbers = [3, 5, 6, 2, 7]
+// const output = []
+// for (i = 0; i < numbers.length; i++) {
+//           const element = numbers[i];
+//           const result = element * element;
+//           output.push(result);
+
+// }
+// console.log(output);
+
+const number  = [5,2,6,4,3]
+
+const bigger = number.filter  (x => x<5 ) ;
+console.log(bigger);
