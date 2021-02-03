daf520b3a4fd413c08c36222f81d97cd906ca02f
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-02-04 01:55:50 +0600

    ajhsdkjhfksah

---
+++ array-split.js
@@ -0,0 +1,11 @@
+// const nums = [1,2,3,4,5,6,7,8]
+// const part =nums.slice(2,5);
+// console.log(part);
+
+const number = [1,2,3,4,5,6,7,8]
+const removed = number.splice(2,3);
+console.log(removed);
+console.log(number);
+
+const together = number.join("ami")
+console.log(together);
