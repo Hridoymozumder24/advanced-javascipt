c5c6f1fac3082881ce7bb4ae5caa0df3365dd3ab
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-02-03 22:46:34 +0600

    mooooooon

---
+++ scope.js
@@ -0,0 +1,17 @@
+
+let bonus = 20;
+function sum(first,second){
+          let result = first+second+bonus;
+          // return result;
+          if(result>9){
+                var mood ='i am happy'  
+                  console.log(mood);
+          }
+          else{
+                    console.log('i am not so good');
+                    
+          }
+          
+}
+const output = sum(7,5);
+// console.log(output);
